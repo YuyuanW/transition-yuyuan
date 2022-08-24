bd21c408210083a89c1e1b46784b953b397f7d1f
unknown <1413733614@qq.com>
2022-08-24 22:10:08 +0800

    可以翻译了，不过返回的结果是utf-8，我需要中文啊

--- src/cli.ts
+++ src/cli.ts
@@ -14,7 +14,4 @@ program
     
 program.parse(process.argv);
 
-const a= [
-    'http://api.fanyi.baidu.com/api/trans/vip/translate?q=banana&from=en&to=zh&appid=20220508001208209&salt=1435660288&sign=3ca61017ca185101111b538aad40ee5c',
-    'http://api.fanyi.baidu.com/api/trans/vip/translate?q=apple&from=en&to=zh&appid=2015063000000001&salt=1435660288&sign=f89f9594663708c1605f3d736d01d2d4'
-]
+
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,35 @@
 import { IncomingMessage } from "http";
 
+const querystring = require('querystring');
 const https = require('https');
+
+const md5 = require('js-md5');
+const iconv = require('iconv-jschardet');
+// iconv.encode(SarchName, 'gbk');
 
 export const translate = (word:string)=>{
   console.log('fuck',word)
+  const q=word
+  const from = 'en'
+  const to = 'zh'
+  const appid  = '20220508001208209'
+  const salt = '1435660288'
+  const code = 'I7Me033g5kN7DjZ9rDnZ'
+  const sign = md5(appid+q+salt+code)
+  
+  const queryList:string = querystring.stringify({ 
+    q,from,to,appid,salt,code,sign
+  });
   const options = {
-    hostname: 'www.baidu.com',
+    hostname: 'api.fanyi.baidu.com',
     port: 443,
-    path: '/',
-    method: 'GET'
+    path: '/api/trans/vip/translate/?'+queryList,
+    method: 'GET',
   };
-  
+
+
   const req = https.request(options, (res:IncomingMessage) => {
-    
-    console.log('statusCode:', res.statusCode);
-    console.log('headers:', res.headers);
-  
+    console.log('header',res.headers)
     res.on('data', (d) => {
       process.stdout.write(d);
     });
@@ -25,5 +39,8 @@ export const translate = (word:string)=>{
     console.error(e);
   });
 
+  // req.setHeader('Content-type','application/json;charset=gbk')
+
   req.end();
 }
+
